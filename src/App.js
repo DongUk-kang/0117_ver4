956e3c1e3c6fceea576602f19b395bbb3ca278d4
dong uk <gusrn1255@naver.com>
2021-01-17 17:43:06 +0900

    ver04 map ver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    //1. 데이터 담을 공간
+    const [loans, setLoans] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //3. 네트워킹 함수
+    const getData = async () => {
+        return (
+            await axios.get('https://api.kivaws.org/v1/loans/newest.json')
+                // .then(aaa => console.log(aaa.data.loans))
+                .then(aaa => {
+                    setLoans(aaa.data.loans)
+                    setLoading(false)
+
+                })
+                .catch(err => console.log(err))
+        )
+    }
+
+    //2. 자동실행함수
+    useEffect(() => {
+        getData()
+    },[])
+
+
+    return (
+        <div>
+            {loans.map(loan => (
+                <>
+                    <h1>name : {loan.name}</h1>
+                    <h2>activity : {loan.activity}</h2>
+                    <h3>coutry : {loan.location.country}</h3>
+                    {loan.description.languages.map(language => (
+                        <h2>languages : {language}</h2>
+                    ))}
+                </>
+
+
+            ))}
+        </div>
+    );
+};
+
+
+
 
 export default App;
